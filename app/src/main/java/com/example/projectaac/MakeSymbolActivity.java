1d8d0160d1b42e4fd28170a146114ad72d9d4d74
JHmin95 <tesla52@naver.com>
2020-11-21 04:00:21 +0900

    상징제작 수정

--- app/src/main/java/com/example/projectaac/MakeSymbolActivity.java
+++ app/src/main/java/com/example/projectaac/MakeSymbolActivity.java
@@ -1,14 +1,64 @@
 package com.example.projectaac;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MakeSymbolActivity extends AppCompatActivity {
+import java.io.InputStream;
 
+public class MakeSymbolActivity extends Activity {
+
+    private static final int REQUEST_CODE =0;
+    private ImageView SelectedImage;
+    private Button btn_gallery, btn_search, btn_AddSymbol;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_symbol);
+
+        SelectedImage = (ImageView)findViewById(R.id.SelectedImage);
+        btn_gallery = (Button)findViewById(R.id.btn_gallery);
+        btn_search = (Button)findViewById(R.id.btn_search);
+        btn_AddSymbol = (Button)findViewById(R.id.btn_AddSymbol);
+
+
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+        btn_AddSymbol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                try{
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    SelectedImage.setImageBitmap(img);
+                }catch(Exception e){}
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
