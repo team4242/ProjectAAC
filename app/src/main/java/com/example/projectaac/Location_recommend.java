23f0a2dbcabd5b62ffcb2ce60e4bc7c5324b29ed
Chisung-Noa <71434791+Chisung-Noa@users.noreply.github.com>
2020-11-30 13:44:20 +0900

    google_map_update

--- app/src/main/java/com/example/projectaac/Location_recommend.java
+++ app/src/main/java/com/example/projectaac/Location_recommend.java
@@ -1,16 +1,29 @@
 package com.example.projectaac;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class Location_recommend extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class Location_recommend extends FragmentActivity implements OnMapReadyCallback {
+
+    private FragmentManager FragmentManager;
+    private MapFragment mapFragment;
     Button btn_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +37,22 @@ public class Location_recommend extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        FragmentManager = getFragmentManager();
+        mapFragment = (MapFragment)FragmentManager.findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void onMapReady(GoogleMap googleMap){
+        LatLng location = new LatLng(36.369042, 127.346312);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("충남대학교");
+        markerOptions.snippet("대학교");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
